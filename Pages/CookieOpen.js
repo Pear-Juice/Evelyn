91b146c771bae7000857186085a327d2953beec0
1eve21lyn8 <71198540+1eve21lyn8@users.noreply.github.com>
2022-03-24 00:11:08 -0600

    UI

--- Pages/CookieOpen.js
+++ Pages/CookieOpen.js
@@ -4,7 +4,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {useEffect, useState} from "react";
 
 function CookieOpenScreen({navigation}) {
-    const fortunes = ["Your eyes are really pretty","Fluffy hair !!"];
+    
+    const fortunes = [
+        "You are beautiful",
+        "You smell really good",
+        "You\'re my light during dark times",
+        "You\'re my rock",
+        "You always know, and say, exactly what I need to hear when I need to hear it",
+        "Thank you for being with me",
+        "I bet you do the crossword puzzle in ink",
+        "You\'re my best friend",
+        ];
 
     useEffect(() => {
         setFortune(fortunes[Math.floor(Math.random() * fortunes.length)]);
@@ -27,7 +37,7 @@ function CookieOpenScreen({navigation}) {
                 <Text>{"Start Over"}</Text>
             </TouchableHighlight>
 
-            <Image resizeMode={'contain'} style={{width: 200, height: 200}} source={require( "../assets/brokenFortuneCookie.png")}/>
+            <Image resizeMode={'contain'} style={{width: 150, height: 150}} source={require( "../assets/Cookie_Open.png")}/>
 
 
         </View>
@@ -40,22 +50,22 @@ export default CookieOpenScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ffbfa3',
+        backgroundColor: '#230D06',
         alignItems: 'center',
         justifyContent: 'center',
     },
 
     button: {
-        backgroundColor: '#f79a7e',
+        backgroundColor: '#F97339',
         margin: 10,
         padding: 10,
-        borderRadius: 10,
+        borderRadius: 20,
     },
 
     fortune: {
-        backgroundColor: '#fae8e3',
+        backgroundColor: '#DBA794',
         width: 300,
-        height: 100,
+        height: 80,
         fontSize: 15,
         fontFamily: 'Courier New',
         margin: 10,
