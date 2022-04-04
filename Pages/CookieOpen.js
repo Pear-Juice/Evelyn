f0646278aa08610943650827c254daab2adc26ad
1eve21lyn8 <71198540+1eve21lyn8@users.noreply.github.com>
2022-04-04 09:10:55 -0600

    styling

--- Pages/CookieOpen.js
+++ Pages/CookieOpen.js
@@ -2,6 +2,7 @@ import {TouchableHighlight, StyleSheet, Text, View, Image, Button} from 'react-n
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {useEffect, useState} from "react";
+import styles from './Colors';
 
 function CookieOpenScreen({navigation}) {
     
@@ -47,32 +48,4 @@ function CookieOpenScreen({navigation}) {
 export default CookieOpenScreen;
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#230D06',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
 
-    button: {
-        backgroundColor: '#F97339',
-        margin: 10,
-        padding: 10,
-        borderRadius: 20,
-    },
-
-    fortune: {
-        backgroundColor: '#DBA794',
-        width: 300,
-        height: 80,
-        fontSize: 15,
-        fontFamily: 'Courier New',
-        margin: 10,
-        padding: 10,
-        borderRadius: 3,
-
-        textAlign: 'center',
-        justifyContent: 'center',
-    }
-});
